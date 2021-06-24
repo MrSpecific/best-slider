4946899b32e9c257287edbd2ef3bec2251b3779b
Will Christenson <will@wlcr.io>
2021-06-24 11:28:21 -0700

    Add callbacks queue

--- src/index.js
+++ src/index.js
@@ -4,19 +4,38 @@
 const bestSlider = (args = {}) => {
   let config = {
     thing: "Thing",
+    initialSlide: 1,
   };
 
   let state = {
     activeSlide: null,
   };
 
+  let callbacks = [];
+
+  const setSlide = (slide = config.initialSlide) => {
+    state.activeSlide = Math.random();
+
+    doCallbacks();
+  };
 
   // Initialize
   const init = () => {
     // Merge passed options with default configuration
     config = { ...config, ...args };
 
+    doCallbacks();
     return state;
+  };
+
+  const addCallback = (newCallback) => {
+    callbacks.push(newCallback);
+  };
+
+  const doCallbacks = () => {
+    if (callbacks.length) {
+      callbacks.forEach((callback) => callback(state));
+    }
   };
 
   if (!init()) return false;
@@ -24,7 +43,10 @@ const bestSlider = (args = {}) => {
   return {
     config,
     state,
+    callbacks,
+    addCallback,
     init,
+    setSlide,
   };
 };
 
